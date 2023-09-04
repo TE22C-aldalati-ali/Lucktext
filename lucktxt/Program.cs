1ec75190d7370f89e8d033d47a31f6987dff0ac9
ali aldalati <ali.aldalati@elev.ga.ntig.se>
2023-09-04 09:31:57 +0200

    oiugyuogyuioyg

---
+++ lucktxt/Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+string name;
+string age;
+string color;
+
+Console.WriteLine("Hej vad heter du?");
+
+ name= Console.ReadLine();
+
+Console.WriteLine("Hur gammal är du?");
+
+age= Console.ReadLine();
+
+Console.WriteLine("Vad är din favorit färg?");
+
+color= Console.ReadLine();
+
+Console.WriteLine(color + age + name);
+
+Console.ReadLine();
+
